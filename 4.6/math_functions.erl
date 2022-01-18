6cfb9bc67f6f15dbed69bf0b6aefa190e50ad22f
Yi wan <1085337513@qq.com>
2022-01-18 10:51:35 +0800

    第四章重做修正.

--- 4.6/math_functions.erl
+++ 4.6/math_functions.erl
@@ -1,25 +1,21 @@
 %%(6) 向math_functions.erl添加一个名为filter(F, L)的高阶函数，它返回L里所有符合条件的元素X（条件是F(X)为true）。
 
 -module(math_functions).
--export([even/1,odd/1,filter/2,init/1]).
+-export([even/1
+		,odd/1,
+		split/1]).
 
 
-init([]) -> {ok,ets:new(?MODULE,[])}.
 even(X)->
-	case X rem 2=:=0 of
-	true->true;
-	false->false end.
+	X rem 2 =:= 0.
 		
 odd(X)->
-	case X rem 2=:=1 of
-	true -> true end.	
+	X rem 2 =:= 1.	
 
 
 
-filter(F, L)->
-	Even=lists:filter(F,L),
-	Odd=lists:filter(F,L),
-	{Even,Odd}.
+split(List) ->
+	{lists:filter(fun(X) -> even(X) end, List), lists:filter(fun(X) -> odd(X) end, List)}.
 
 %%这一题的关键点是在上一题的基础上加了一个高阶函数filter(F,L).
 %%记得fun()的格式fun(参数)->参数... end.
